0171d8f6863bcc11dbaf461385fb19d0e1cdaec0
simran <malwinder3299148@gmail.com>
2026-03-27 12:36:16 +0530

    1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import './index.css'
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
-import About from './pages/About.jsx'
+import About from './pages/About.jsx';
+import Setting from './pages/Setting.jsx';
+
 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
@@ -13,7 +15,10 @@ createRoot(document.getElementById('root')).render(
     <Route path='/Home' element={<Home/>}/>
     <Route path='/Login' element={<Login/>}/>
     <Route path='/Register' element={<Register/>}/>
-        <Route path='/About' element={<About/>}/>
+    <Route path='/About' element={<About/>}/> 
+     <Route path='/Setting' element={<Setting/>}/>
+
+
 
  </Routes>
  </BrowserRouter>
